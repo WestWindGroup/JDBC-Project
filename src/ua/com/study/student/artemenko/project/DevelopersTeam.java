ad4d33722527af0b694970090b897f7cd6c2f8ad
Evgen Artemenko <artemenkojenia.ea@gmail.com>
2016-09-15 22:28:40 +0300

    end

--- src/ua/com/study/student/artemenko/project/DevelopersTeam.java
+++
@@ -1,9 +0,0 @@
-package ua.com.study.student.artemenko.project;
-
-import java.util.Map;
-
-public class DevelopersTeam extends Team {
-    public DevelopersTeam(Map<Integer, Staff> team) {
-        super(team);
-    }
-}
--- src/ua/com/study/student/artemenko/project/TestersTeam.java
+++
@@ -1,9 +0,0 @@
-package ua.com.study.student.artemenko.project;
-
-import java.util.Map;
-
-public class TestersTeam extends Team {
-    public TestersTeam(Map<Integer, Staff> team) {
-        super(team);
-    }
-}
